c39a6223bc83b8681e6f5007fb9f48254638f97d
Hammad <Hammad90191@gmail.com>
2025-07-24 13:08:51 +0500

    updated userslice

--- src/store/user/userSlice.js
+++ src/store/user/userSlice.js
@@ -13,6 +13,7 @@ export const fetchUser = createAsyncThunk(
       );
       return response.data;
     } catch (error) {
+      console.log('first', error.response?.data)
       return thunkAPI.rejectWithValue(error.response?.data || error.message);
     }
   }
